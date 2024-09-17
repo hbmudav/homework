a67450e5425abe1a5b2c382ea5099645812888d6
hbmudav <udav-hbm@mail.ru>
2024-09-17 21:40:31 +0900

    Add files via upload

---
+++ lesson1.py
@@ -0,0 +1,11 @@
+# 1st program
+print(9**0.5*5)
+# 2nd program
+print(9.99>9.98 and 1000!=1000.1)
+# 3rd program
+print(2*2+2 == 2*(2+2))
+# 4th program
+name = '123.456'
+print(type(name))
+print(float('123.456'),((int(123.456*10)%1000)%100)%10)
+print(int(float('123.456') * 10) % 10)
