23a6c25c52e853192f0655c19882a316082ea05d
hbmudav <udav-hbm@mail.ru>
2024-09-23 00:03:35 +0900

    Add files via upload

--- variables.py
+++ variables.py
@@ -1,5 +1,5 @@
-quantity_tasks = 15.0
+quantity_tasks = 12
 quantity_hours = 1.5
 name_of_course = 'Python'
-average_execution_time = (1.5/15)
+average_execution_time = (1.5/12)
 print('Курс:',name_of_course,' всего задач:',quantity_tasks,' затрачено часов:',quantity_hours,' среднее время выполнения',average_execution_time)
