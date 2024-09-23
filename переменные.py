4bcbc3f7c2453e4eba4253410ae66b9930b102ba
hbmudav <udav-hbm@mail.ru>
2024-09-23 14:04:49 +0900

    Add files via upload

---
+++ переменные.py
@@ -0,0 +1,5 @@
+homework = 12
+time = 1.5
+course = 'Python'
+task_time = (time/homework)
+print('Курс ',course, 'всего задач',homework, 'затрачено часов',time, 'среднее время выполнения',task_time,'часа')
