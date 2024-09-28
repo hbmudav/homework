b08d4c14cf61099efd095e7a1d21771624fe7eae
hbmudav <udav-hbm@mail.ru>
2024-09-28 20:36:59 +0900

    Add files via upload

--- average score.py
+++ average score.py
@@ -4,10 +4,13 @@ a = [sum(grades[0])/len(grades[0]),sum(grades[1])/len(grades[1]),sum(grades[2])/
      sum(grades[4])/len(grades[4])]
 b = sorted(students)
 print(dict(zip(b,a)))
+
+s = []
+for i in grades:
+    c =  sum(i)/len(i)
+    s.append(c)
+print(dict(zip(b,s)))
 # avg =[sum(sublist) for sublist in grades]
-# s = 0
-# for i in grades:
-#     res1 += i
 # average = s / len(grades[0:4])
 # print(average)
 # for i in range(s):
