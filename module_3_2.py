37823380adc0214614a632d08f8dd268d708619a
hbmudav <udav-hbm@mail.ru>
2024-10-08 21:40:22 +0800

    Add files via upload

--- module_3_2.py
+++ module_3_2.py
@@ -1,5 +1,4 @@
-# def send_email (massege, recipient, *, sender = 'university.help@gmail.com'):
-def send_email (massege, recipient, *, sender = 'urban.teacher@mail.ru'):
+def send_email (massege, recipient, *, sender = 'university.help@gmail.com'):
     if ('@' or ('.com' or '.ru' or '.net')) not in (recipient and sender):
         print(f'Невозможно отправить письмо с адреса {sender} на адрес {recipient}.')
     elif sender == recipient:
@@ -8,8 +7,7 @@ def send_email (massege, recipient, *, sender = 'urban.teacher@mail.ru'):
         print(f'Письмо успешно отправлено с адреса {sender} на адрес {recipient}.' )
     elif sender != 'university.help@gmail.com':
         print(f'НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}.')
-
-    send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
-    send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
-    send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
-    send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
+send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
+send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
+send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
+send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
