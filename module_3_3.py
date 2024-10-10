a2a5e2da4fbbfb7825ed3df787799990cbe6cdde
hbmudav <udav-hbm@mail.ru>
2024-10-10 21:22:55 +0800

    Add files via upload

---
+++ module_3_3.py
@@ -0,0 +1,10 @@
+def print_params(a = 1, b = 'строка', c = True):
+    print(a, b, c)
+print_params(b = 25)
+print_params(c = [1, 2, 3])
+value_list = [2, [321], 'str']
+value_dict = {'a':4, 'b':5, 'c':6}
+print_params(*value_list)
+print_params(**value_dict)
+value_list2 = [8.7, 'oka']
+print_params(*value_list2, 42)
