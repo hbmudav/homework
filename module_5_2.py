8598cc7a6efecbd91fe03b053f38407c7cb854e6
hbmudav <udav-hbm@mail.ru>
2024-11-02 20:21:02 +0800

    Add files via upload

---
+++ module_5_2.py
@@ -0,0 +1,36 @@
+class House:
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+    def go_to (self, new_floor):
+        if 0 < new_floor <= self.number_of_floors:
+            for floor in range(1, new_floor + 1):
+                print(floor)
+        else:
+            print('Такого этажа не существует')
+    def __len__(self):
+        return self.number_of_floors
+    def __str__(self):
+        return (f'Название: {self.name}, количество этажей: {self.number_of_floors}')
+
+
+h1 = House('ЖК Эльбрус', 30)
+h2 = House('ЖК Горский', 18)
+h3 = House('Домик в деревне', 2)
+print()
+print(h1)
+print(h2)
+print(h3)
+print()
+print(len(h1))
+print(len(h2))
+print(len(h3))
+
+# h1.go_to(5)
+# print('ЖК Эльбрус')
+# print()
+# h2.go_to(10)
+# print('ЖК Горский')
+# print()
+# h3.go_to(10)
+# print('Домик в деревне')
